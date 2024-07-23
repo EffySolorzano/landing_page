4cc9f62811c7a9b99158f5d742af02694f09f5f7
Stephanie Solórzano <stephaniesolorzano@MacBook-Pro-de-Stephanie.local>
2024-07-23 15:13:44 +0100

    updates

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -16,10 +16,9 @@ const LandingPage = () => {
       <div className="profile-section">
       <img src={`${process.env.PUBLIC_URL}/steph.JPG`} alt="Profile" className="profile-pic" />
         <h2 className="title">Stephanie Solórzano</h2>
-        <h3 className="text-left">I'm a Junior Front-End Developer from 🇻🇪 ✨ and located in Europe. I have a passion for creating beautiful 
-        <br/>and functional websites and landing pages. My background is in illustration, which brings a unique artistic perspective 
-        <br/> to web design and development. <br/>Combining my love for art and technology, I craft visually engaging and user-friendly
-        <br/> experiences that captivate and delight. <br/>Explore my work and see how I can bring your digital vision to life.</h3>
+        <h3 className="text-left">I'm a Junior Front-End Developer from 🇻🇪 ✨ located in Europe. I truly enjoy creating  
+        <br/>functional websites and landing pages. My background is in illustration, which brings a unique artistic perspective 
+        <br/> to web design and development.  <br/>Explore my work and see how I can bring your digital vision to life.</h3>
       </div>
       <div className="button-links">
         {links.map((link, index) => (
